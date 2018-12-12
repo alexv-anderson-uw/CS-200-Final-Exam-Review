0935c3bfa2b0bd9593cad1b2e0eceecb96a23a3f
Sandhya Kannan <sandhyakannan@Sandhyas-MacBook-Air.local>
2018-12-11 20:52:32 -0600

    Examples for Scanner and Static vs Instance

--- src/ScannerExample.java
+++ src/ScannerExample.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Demonstrates using a Scanner to parse a string
  * 
  * @author Sandhya
  */
 public class ScannerExample {
-
+	
 	/**
 	 * Entry point for the program
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		System.out.println("Enter a valid name ");
+		name = scanner.next();
 		
+		System.out.println("which is your favorite Pokemon? ");
+		String character = scanner.nextLine();
+		
+		System.out.println(name+"\'s favourite pokemon is "+ character);
+				
+		scanner.close();
 	}
 }
--- src/StaticVsInstance.java
+++ src/StaticVsInstance.java
@@ -3,6 +3,46 @@
  * 
  * @author Sandhya
  */
+class Car{
+	private static int CAR_COUNT=0;
+	private String model;
+	private String carNumber;
+	
+	Car(){
+		updateCarCount();
+	}
+	
+	Car(String model, String number){
+		this.model = model;
+		carNumber = number;
+		updateCarCount();
+	}
+	
+	public static int getCarCount() {
+		return CAR_COUNT;
+	}
+	
+	private static void updateCarCount() {
+		CAR_COUNT++;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public void setCarNumber(String number) {
+		carNumber = number;
+	}
+	
+	public String getCarNumber() {
+		return this.carNumber;
+	}
+	
+}
 public class StaticVsInstance {
 
 	/**
@@ -10,6 +50,16 @@ public class StaticVsInstance {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		Car car1 = new Car();
+		car1.setModel("Chrysler");
+		car1.setCarNumber("WI-1053 A");
+		System.out.println("Car 1 - number : " + car1.getCarNumber());
+		
+		Car car2 = new Car("Subaru", "MN-4453 B");
+		System.out.println("Car 2 - model : " + car2.getModel());
+		
+		System.out.println("Number of cars : " + Car.getCarCount());
 		
 	}
 }
