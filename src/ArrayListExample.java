fb0ac6c29bb636c36a6839f31b75ff793ea341fa
T.J <tk100794@gmail.com>
2018-12-13 01:36:22 -0600

    Added ArrayListExample and MethodSignatureAndOverloading

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Demonstrates using various methods on the ArrayList class
  * 
@@ -10,6 +12,83 @@ public class ArrayListExample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// Create ArrayList for maximum fun
+		// ArrayList<char[]> funList = new ArrayList<char[]>(Integer.MAX_VALUE);
+		ArrayList<char[]> funList = new ArrayList<char[]>(0);
+		printMyList(funList);
+
+    // Are you ready to get started?
+		funList.add(new char[] { 'm', '8', 'T' });
+		funList.add(0, new char[] { 'a', 'y', 'e' });
+
+		// printMyList(funList);
+
+
+		// Let's change our list
+		funList.get(1)[2] = 't';
+		char[] liveOn = funList.get(0);
+		funList.set(0, new char[] { 'g', '\'', 'd', 'a', 'y' });
+
+		// printMyList(funList);
+
+
+		// Re-addity
+		funList.add(1, funList.get(1));
+
+		// printMyList(funList);
+
+
+		// Pointers matter!
+		funList.get(2)[1] = 'a';
+		funList.add(0, new char[5]);
 		
+		// printMyList(funList);
+
+
+		// Pointers still matter!!
+		funList.remove(funList.size() - 1);
+		char[] someWord = funList.get(1);
+		someWord[0] = 'h';
+		someWord[2] = 'z';
+		excitify(someWord);
+
+		// printMyList(funList);
+
+
+		// Affirmative
+		funList.add(1, liveOn);
+		funList.set(0, liveOn);
+		// Some nonsense
+		funList.remove(funList.get(funList.indexOf(funList.get(3))));
+
+		// printMyList(funList);
+	}
+
+	public static void excitify(char[] letters) {
+		letters[letters.length - 1] = '!';
+	}
+
+	public static ArrayList<char[]> mutagen(ArrayList<char[]> original) {
+		ArrayList<char[]> mutant = copy(original);
+
+		mutant.add(new char[] { 5, 10 });
+
+		return mutant;
+	}
+
+	// Return a shallow copy of the original ArrayList
+	public static ArrayList<char[]> copy(ArrayList<char[]> original) {
+		return new ArrayList<char[]>(original);
+	}
+
+	static void printMyList(ArrayList<char[]> words) {
+		System.out.print("[");
+		int count = 0;
+		for (char[] word : words) {
+			if (count++ > 0)
+				System.out.print(", ");
+			System.out.print(word);
+		}
+		System.out.println("]");
 	}
 }
--- src/MethodSignatureAndOverloading.java
+++ src/MethodSignatureAndOverloading.java
@@ -10,6 +10,157 @@ public class MethodSignatureAndOverloading {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		int x = 2;
+		double y = 2.5;
+
+		// Section 1, the basics
+		// What methods get called here?
+		System.out.println(basics(x));
+		System.out.println(basics((int)y));
+		System.out.println(basics(y));
+		System.out.println(basics((double)x));
+
+		// What about here?
+		// System.out.println(basics(basics(y));
+
+		// Section 2, fun with constructors
+		// A bit more complicated
+		// Namer test1 = buildNamer(x);
+		// Namer test2 = buildNamer((double)x);
+
+		// System.out.println(test1.getName());
+		// System.out.println(test2.getName());
+
+		// Section 3, confusion
+		// System.out.println(confusion(x, y));
+		// System.out.println(confusion(y, x));
+
+		// What happens with this one?
+		// System.out.println(confusion(x, x));
+
+
+		// Section 4, extras
+		// Read the comment on the first overload method
+		// 	for more info
+		// String loaded = overload(x, x, x);
+		// System.out.println(loaded);
+	}
+
+	// Section 1, the basics
+	public static int basics(double input) {
+		return (int)input + 1;
+	}
+
+	public static double basics(int input) {
+		return input;
+	}
+
+	// What if we add this one? Does the output change?
+	// public static double basics(double input) {
+	// 	return input;
+	// }
+
+
+	// Section 2, fun with constructors
+	// Let's do multiple overloads
+	// public static Namer buildNamer(int x) {
+	// 	return new Namer();
+	// }
+
+	// public static Namer buildNamer(double x) {
+	// 	return new Namer("Double: " + x);
+	// }
+
+
+	// Section 3, confusion
+	// Can you guess which one is picked?
+	// public static String confusion(int x, double y) {
+	// 	return "Called confusion(int, double)";
+	// }
+
+	// public static String confusion(double x, int y) {
+	// 	return "Called confusion(double, int)";
+	// }
+
+	// What if we include this one? Does the output change?
+//	public static String confusion(int x, int y) {
+//		return "Called confusion(int, int)";
+//	}
+	
+
+	// Section 4, extras
+	/** 
+	 * The next part is a weird extra that you don't need to know but
+	 * might find interesting
+	 *
+	 * In the following, some really weird stuff happens with what java considers 
+	 *	to be ambiguous. If you try to call these methods with
+	 *	overload(0, 0, 0); then some are ambiguous, but not all
+	 *	of them. Let me know if you figure out the rule, it baffles me.
+	 **/
+	/** ----------- EXTRA START ----------- **/
+//	public static String overload(double x, int y, double z) {
+//		return "Got double, int, double";
+//	}
+//
+//	public static String overload(double x, double y, int z) {
+//		return "Got double, double, int";
+//	}
+//
+//	public static String overload(int x, int y, double z) {
+//		return "Got int, int, double!!";
+//	}
+//
+//	public static String overload(int x, double y, double z) {
+//		return "Got int, double, double!!";
+//	}
+//
+//	public static boolean overload(double x, int y, int z) {
+//		return false;
+//	}
+	/** This is the end of the extra stuff! **/
+	/** ------------ EXTRA END ------------ **/
+}
+
+class Namer {
+	private String name = "the default";
+	
+	public Namer() {
+		makePlural(name);
+	}
+
+	Namer(String name) {
+		setName(name);
+	}
+
+//	Namer() {
+//		this("No-param!");
+//	}
+//
+//	private	Namer(String name) {
+//		this.name = name;
+//	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		// Only allow non-null, non-empty names 
+		if (name == null || name.length() < 1) {
+			return;
+		}
+
+		this.name = name;
+	}
+
+	public void makePlural() {
+		name = makePlural(name);
+	}
+
+	// What would happen if you remove the name param here?
+	public static String makePlural(String name) {
+		// What happens if I use this.name?
+		return name + "s";
 	}
 }
